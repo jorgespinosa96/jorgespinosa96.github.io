5c8e829ebbd62004325733ae54cdaea725cd0856
jorgespinosa <61994064+jorgespinosa@users.noreply.github.com>
2022-05-27 19:07:50 -0500

    creacion de funciones para bd

--- INDEXDB/js/main.js
+++ INDEXDB/js/main.js
@@ -1,7 +1,43 @@
-//create database
-const db=new Dexie('myBbStore')
-db.version(1).stores({
-    friends:'name,age'
-})
+import productdb,{
+    bulkcreate
+} from "./Module.js";
 
-db.open
+
+let db=productdb("Productdb",{
+    products:'++id,name,seller,price'
+});
+
+//input tags
+const userid=document.getElementById('userid');
+const proname=document.getElementById('proname');
+const seller=document.getElementById('seller');
+const price=document.getElementById('price');
+
+//buttons
+const btncreate=document.getElementById("btn-create");
+const btnread=document.getElementById("btn-read");
+const btnupdate=document.getElementById("btn-update");
+const btndelete=document.getElementById("btn-delete");
+
+//insert value using create button
+btncreate.onclick=(event)=>{
+   let flag= bulkcreate(db.products,{
+        name: proname.value,
+        seller:seller.value,
+        price:price.value
+    })
+//console.log(flag);
+
+proname.value=seller.value=price.value="";
+getData();
+}
+
+const  getData=()=>{
+    let index=0;
+    let obj={};
+
+    db.products.count((count)=>){
+        console.log(count);
+    }
+
+}
